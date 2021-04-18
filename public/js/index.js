b595c403c25f46985be29881c9ebcb4fe8006f95
byeongwan <81760347+byeongco@users.noreply.github.com>
2021-04-18 21:14:28 +0900

    메인 화면 개발 (#10)
    * #7 메인 화면 개발
    - header, footer.pug 개발
    - index.pug 레이아웃 개발
    
    * #7 메인 화면 개발
    - header, footer.pug 코드 layout.pug에 합침
    
    * #7 메인 화면 개발
    - index 화면 Top 영역 레이아웃 구성
    - 관련 image 파일 첨부
    
    * #7 메인 화면 개발
    - top 영역 레이아웃 및 애니메이션 구현
    - notice 레이아웃 구성
    - reward 레이아웃 진행 중
    
    * #7 메인 화면 개발
    - bean, brazil, fav, reserve, store 레이아웃 및 애니메이션 적용
    
    Co-authored-by: KimSatGat <kimsatgat22@gmail.com>

---
+++ public/js/index.js
@@ -0,0 +1,53 @@
+const containerTop = document.querySelector('.container__top');
+const topImages = containerTop.querySelectorAll('img');
+const topBtn = containerTop.querySelector('button');
+let offset = 0;
+setTimeout(() => {
+  topImages[0].classList.add('active');
+}, 500);
+for(let ix = 1; ix < topImages.length; ix += 2) {
+  offset += 1;
+  setTimeout(() => {
+    topImages[ix].classList.add('active');
+    topImages[ix + 1].classList.add('active');
+  }, 500 * offset + 500);
+}
+offset += 1;
+setTimeout(() => {
+  topBtn.classList.add('active');
+}, 500* offset + 500);
+
+const containerBean = document.querySelector('.container__bean');
+const containerFav = document.querySelector('.container__fav');
+const containerReserve = document.querySelector('.container__reserve');
+const containerStore = document.querySelector('.container__store');
+
+window.addEventListener('scroll', () => {
+  const remainedBeanPos = containerBean.getBoundingClientRect().top;
+  const remainedFavPos = containerFav.getBoundingClientRect().top;
+  const remainedReservePos = containerReserve.getBoundingClientRect().top;
+  const remainedStorePos = containerStore.getBoundingClientRect().top;
+
+  if(window.innerHeight > remainedBeanPos) {
+    const beanImg = containerBean.querySelector('.container__bean__wrapper__img');
+    const beanContent = containerBean.querySelector('.container__bean__content');
+
+    beanImg.classList.add('active');
+    beanContent.classList.add('active');
+  }
+  if(window.innerHeight > remainedFavPos) {
+    const favLeft = containerFav.querySelector('.container__fav__left');
+
+    favLeft.classList.add('active');
+  }
+  if(window.innerHeight > remainedReservePos) {
+    const reserveRight = containerReserve.querySelector('.container__reserve__right');
+
+    reserveRight.classList.add('active');
+  }
+  if(window.innerHeight > remainedStorePos) {
+    const storeRight = containerStore.querySelector('.container__store__right');
+
+    storeRight.classList.add('active');
+  }
+});
