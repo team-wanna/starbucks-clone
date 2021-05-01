051c333e1e2f445cdfa9f30ad183c3663df8883f
Hanna Lee <8annahxxl@gmail.com>
2021-05-02 01:14:51 +0900

    css, js src layout.pug로 이동, header toggle 개발

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,8 @@ import User from "../models/User";
 
 // Get
 export const output = {
-  join: (req, res) => res.render("join"),
-  login: (req, res) => res.render("login"),
+  join: (req, res) => res.render("join", { title: "join" }),
+  login: (req, res) => res.render("login", { title: "login" }),
   my: (req, res) => res.send("my page"),
 };
 
@@ -14,6 +14,7 @@ export const process = {
       const {
         id,
         pwd,
+        pwd2,
         name,
         gender,
         year,
@@ -24,19 +25,25 @@ export const process = {
         email,
         nickname,
       } = req.body;
-      const birth = String(year) + String(month) + String(day) + calendar;
-      // 회원 생성 후 로그인 페이지 이동
-      const user = await User({
-        id,
-        name,
-        gender,
-        birth,
-        phone,
-        email,
-        nickname,
-      });
-      await User.register(user, pwd);
-      res.redirect("/user/login");
+
+      if (pwd != pwd2) {
+        // 추가 구현 필요함
+        res.status(400).send("비밀번호 불일치");
+      } else {
+        const birth = String(year) + String(month) + String(day) + calendar;
+        // 회원 생성 후 로그인 페이지 이동
+        const user = await User({
+          id,
+          name,
+          gender,
+          birth,
+          phone,
+          email,
+          nickname,
+        });
+        await User.register(user, pwd);
+        res.redirect("/user/login");
+      }
     } catch (err) {
       console.log(err);
     }
