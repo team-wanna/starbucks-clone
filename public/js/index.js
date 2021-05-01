540d9270c435a549894b3625e89a5a3edebf392b
Hanna Lee <8annahxxl@gmail.com>
2021-05-02 01:44:24 +0900

    header toggle 개발, src(css,js) layout.pug로 이동

--- public/js/index.js
+++ public/js/index.js
@@ -57,26 +57,3 @@ window.addEventListener("scroll", () => {
     storeRight.classList.add("active");
   }
 });
-
-// header__toggle
-// 토글 애니메이션
-const headerNavs = document.querySelectorAll(".header__gnb_nav__item");
-const headerToggles = document.querySelectorAll(".header__toggle__container");
-
-for (let headerNav of headerNavs) {
-  let headerToggle = headerToggles[Array.from(headerNavs).indexOf(headerNav)];
-  headerNav.addEventListener("mouseenter", () => {
-    headerToggle.classList.add("active");
-  });
-  headerToggle.addEventListener("mouseenter", () => {
-    headerToggle.classList.add("active");
-    headerNav.classList.add("nav-active");
-  });
-  headerNav.addEventListener("mouseleave", () => {
-    headerToggle.classList.remove("active");
-  });
-  headerToggle.addEventListener("mouseleave", () => {
-    headerToggle.classList.remove("active");
-    headerNav.classList.remove("nav-active");
-  });
-}
